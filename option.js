c6d629f6f291d6b9f9d5d8a163ddf193b791b474
Veerpal Brar <VeerpalBrar@users.noreply.github.com>
2016-05-27 12:37:31 -0400

    create the add_site function

--- option.js
+++ option.js
@@ -1,8 +1,13 @@
 function add_site(){
   var site_url = $('#site_name').val();
+  site_url = format_url(site_url)
   if(!check_url_valid(site_url)){
-    sire_url = fix_url()
+    alert("Sorry, that is not a valid website!")
+    return
   }
-  //message background js to number of sites?
-  // add site and then update number of sites
+  chrome.runtime.sendMessage({add_site: site_url}, function(response) {
+    if(response.action_taken == "added"){
+      alert("Added!");
+    }
+  });
 }
