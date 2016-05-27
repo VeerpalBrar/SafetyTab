fbc895909c442cf9e16a94b1705249d8269e8058
Veerpal Brar <VeerpalBrar@users.noreply.github.com>
2016-05-27 12:19:38 -0400

    Update option.js

--- option.js
+++ option.js
@@ -1,3 +1,8 @@
 function add_site(){
-  site_url = 
+  var site_url = $('#site_name').val();
+  if(!check_url_valid(site_url)){
+    sire_url = fix_url()
+  }
+  //message background js to number of sites?
+  // add site and then update number of sites
 }
