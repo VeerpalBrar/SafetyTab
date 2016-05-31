b002bcce53aac1475de996432a9d3307cab112e4
Veerpal Brar <VeerpalBrar@users.noreply.github.com>
2016-05-31 13:41:01 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,48 +1,69 @@
 //on browers_action click, open a new tab using the url stored under "site_url"
 // then store a new url for the next browser click. 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.storage.sync.get('site_url', function(err, site) {
-    if(err) return "https://www/google.ca"
-  chrome.tabs.create({"url": site});
-  store_url()
+    chrome.storage.sync.get('site_url', function(err, site) {
+        if (err){
+             chrome.tabs.create({"url": "http://www.google.ca"
+			});
+        } 
+        else{
+			chrome.tabs.create({
+            "url": site
+        });
+		}
+        store_url()
+    })
 });
 
 // on window open get a site url stored under the "site_url" key
 chrome.windows.onCreated.addListener(function() {
     store_url();
-})
+});
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) 
-    if (request.add_site){
-     store_url(request.add_site)
-     sendResponse({action_taken: "added"});
-  }
-  
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.add_site) {
+        store_url(request.add_site)
+        sendResponse({
+            action_taken: "added"
+        });
+    }
+
 });
 
 // get the total number of "safe" sites that user has chosen
 // google homepage is the default site
-function get_num_of_sites(){
-  return chrome.storage.sync.get('num_of_sites', function(err, num) {
-    if(err){
-      chrome.storage.sync.set({'num_of_sites':1})
-      chrome.storage.sync.set({'site_1':"https://www.google.ca"})
-    }
-    return num
-}
+function get_num_of_sites() {
+    return chrome.storage.sync.get('num_of_sites', function(err, num) {
+        if (err) {
+            chrome.storage.sync.set({
+                'num_of_sites': 1
+            })
+            chrome.storage.sync.set({
+                'site_1': "https://www.google.ca"
+            })
+			console.log(1)
+			return 1
+        }
+		console.log(num)
+        return num
+    })
+};
 
 // given the total number of site, get the url for one site randomly
 // google homepage is default
-function get_site_url(num_of_sites){
-  var site_number = Math.floor(Math.random() * num_of_sites);
-  var site_key = "site_"+site_number.toString()
-  chrome.storage.sync.get(site_key, function(err, site_url) {
-    if(err)return "https//www.google.ca"
-      return site_url
-  });
-}
+function get_site_url(num_of_sites) {
+    var site_number = Math.floor(Math.random() * num_of_sites);
+    var site_key = "site_" + site_number.toString()
+    chrome.storage.sync.get(site_key, function(err, site_url) {
+        if (err) return "https//www.google.ca"
+        return site_url
+    });
+};
 
-function store_url(){
-  var num = get_num_of_sites()
-  chrome.storage.sync.set({'site_url':get_site_url(num)})
-}
+function store_url() {
+    var num = get_num_of_sites()
+	console.log(num)
+    chrome.storage.sync.set({
+        'site_url': get_site_url(num)
+    })
+};
