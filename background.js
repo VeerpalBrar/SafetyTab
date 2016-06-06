5254a3b008b3e8d7e6b413c290c46a20e58705a8
Veerpal Brar <VeerpalBrar@users.noreply.github.com>
2016-06-06 15:24:56 -0400

    fixed issues with chrome storage
    Now set, and get functions will return the site urls.

--- background.js
+++ background.js
@@ -1,30 +1,32 @@
 //on browers_action click, open a new tab using the url stored under "site_url"
 // then store a new url for the next browser click. 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.storage.sync.get('site_url', function(err, site) {
-        if (err){
-             chrome.tabs.create({"url": "http://www.google.ca"
-			});
-        } 
-        else{
-			chrome.tabs.create({
-            "url": site
-        });
-		}
+    chrome.storage.sync.get('site_url', function(result) {
+        if (!result) {
+            chrome.tabs.create({
+                "url": "http://www.google.ca"
+            });
+        } else {
+            chrome.tabs.create({
+                "url": result.site_url
+            });
+        }
         store_url()
     })
+
 });
 
 // on window open get a site url stored under the "site_url" key
 chrome.windows.onCreated.addListener(function() {
     store_url();
-});
+})
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
     if (request.add_site) {
         chrome.storage.sync.set({
-				'site_url': request.add_site
-			})
+            'site_url': request.add_site
+        })
         sendResponse({
             action_taken: "added"
         });
@@ -32,74 +34,60 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 });
 
-chrome.runtime.onInstalled.addListener(function(details){
-	console.log("starting")
-	 chrome.storage.sync.set({
-				'site_1': "https://google.ca"
-			}, function(){console.log("finished")});
-	 chrome.storage.sync.set({
-				'site_2': "https://stackoverflow.com"
-			}, function(){console.log("finished")})
-	 chrome.storage.sync.set({
-				'site_1': "https://wired.com"
-			}, function(){console.log("finished")})
-	 chrome.storage.sync.set({
-				'site_1': "https://github.com"
-			}, function(){console.log("finished")})
-	chrome.storage.sync.set({
-				'num_of_sites': 4
-			}, function(){console.log("finished")})
-	
+chrome.runtime.onInstalled.addListener(function(details) {
+    var default_site = {}
+    default_site['site_1'] = "https://google.ca";
+    default_site['site_2'] = "https://stackoverflow.com";
+    default_site['site_3'] = "https://wired.com";
+    default_site['site_4'] = "https://github.com";
+    chrome.storage.local.set(default_site, function() {
+        store_url()
+    })
 });
 // get the total number of "safe" sites that user has chosen
 // google homepage is the default site
 function get_num_of_sites(callback) {
-	console.log("called get_num_of_sites")
-    chrome.storage.sync.get('num_of_sites', function(err, num) {
-        if (err) {
+    chrome.storage.sync.get('num_of_sites', function(num) {
+        if (!num) {
+            console.log("ehllo")
             chrome.storage.sync.set({
                 'num_of_sites': 1
             })
             chrome.storage.sync.set({
                 'site_1': "https://www.google.ca"
             })
-			console.log(1)
-			num=1
-		}
-        
-		console.log("not 1", num)
-		if(callback){
-			callback(num)
-		}
-		})
+            num = 1
+        }
+        if (callback) {
+            callback(num.num_of_sites)
+        }
+    })
 };
 
 // given the total number of site, get the url for one site randomly
 // google homepage is default
 function get_site_url(num_of_sites) {
-	console.log("called get_site_url")
-    var site_number = Math.floor(Math.random() * num_of_sites);
+    var site_number = Math.floor(Math.random() * num_of_sites) + 1;
     var site_key = "site_" + site_number.toString()
-	console.log("site_key: ", site_key)
-    chrome.storage.sync.get(site_key, function(err, site_url) {
-        if (err){
-			console.log(err);
-			console.log("error")
-			chrome.storage.sync.set({
-				'site_url': "https://google.ca"
-			})
-		}
-		else{
-			console.log("not error")
-			chrome.storage.sync.set({
-			'site_url': site_url
-			})
-		}
+    var get_item = []
+    get_item.push(site_key)
+    chrome.storage.sync.get(get_item, function(result) {
+        var keys = Object.keys(result);
+        var key = keys[0];
+        console.debug(key + ' = ' + result[key]);
+        if (!result) {
+            chrome.storage.sync.set({
+                'site_url': "https://google.ca"
+            })
+        } else {
+            chrome.storage.sync.set({
+                'site_url': result[key]
+            })
+        }
 
     });
 };
 
 function store_url(url) {
-	console.log("called store_url")
     get_num_of_sites(get_site_url)
 };
