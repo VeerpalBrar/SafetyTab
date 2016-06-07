55a7f0b5de39c4bfb62f1923b029741eb5b3805a
Veerpal Brar <VeerpalBrar@users.noreply.github.com>
2016-06-07 10:18:04 -0400

    added site list functionality

--- option.js
+++ option.js
@@ -1,17 +1,22 @@
 function add_site(){
+	console.log("start")
   var site_url = $('#site_name').val();
-  site_url = format_url(site_url)
+  //site_url = format_url(site_url)
+  console.log(site_url)
   if(!check_url_valid(site_url)){
     alert("Sorry, that is not a valid website!")
     return
   }
-  chrome.runtime.sendMessage({add_site: site_url}, function(response) {
+  chrome.runtime.sendMessage({"add_site": site_url}, function(response) {
     if(response.action_taken == "added"){
       alert("Added!");
+	  remove_children('site_list')
+	  get_urls();
     }
   });
 }
 
+// not complete. placeholder. 
 function check_url_valid(url){
   if(url.charAt(0) == "h") return true
   return false
@@ -19,14 +24,26 @@ function check_url_valid(url){
 
 function get_urls(){
   chrome.storage.sync.get(null, function(items){
-    var all_values = Object.values(items)
+    var all_values = Object.keys(items)
+	console.log(all_values)
     for(var i = 0; i < all_values.length;i++){
-      var newsite = document.createElement('li');
-      newsite.setAttribute('class',"site");
-      newsite.innerHTML = all_values[i]
-      $( ".sites" ).append( newsite );
-    }
+		if(all_values[i]!= 'num_of_sites' && all_values[i]!= 'site_url'){
+		  var newsite = document.createElement('li');
+		  newsite.setAttribute('class',"site");
+		  newsite.appendChild(document.createTextNode(items[all_values[i]]))
+		  $( ".sites" ).append( newsite );
+		}
+	}
   })
 }
 
-document.addEventListener('DOMContentLoaded', get_urls);
+function remove_children(element_id){
+	var myNode = document.getElementById(element_id);
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+}
+window.addEventListener("load", get_urls);
+document.getElementById('save').addEventListener('click',
+    add_site);
+
