294554d31e4c716a049aa641ea00004a630215bf
Veerpal Brar <VeerpalBrar@users.noreply.github.com>
2016-06-10 11:26:33 -0400

    add more message passing

--- background.js
+++ background.js
@@ -2,36 +2,52 @@
 // then store a new url for the next browser click. 
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.storage.sync.get('site_url', function(result) {
-        if (!result) {
+        if (chrome.runtime.lastError) {
             chrome.tabs.create({
-                "url": "http://www.google.ca"
+                "url": "https://en.wikipedia.org/wiki/Year_Without_a_Summer"
             });
         } else {
             chrome.tabs.create({
                 "url": result.site_url
             });
         }
-        store_url()
+        get_site_url();
     })
-
 });
 
 // on window open get a site url stored under the "site_url" key
 chrome.windows.onCreated.addListener(function() {
-    store_url();
+    get_site_url();
 })
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.add_site) {
         chrome.storage.sync.get('sites', function(result) {
+			if (chrome.runtime.lastError){
+				sendResponse({action_taken: "error"});
+				return false
+			}
             var site_list = result.sites
             site_list.push(request.add_site)
-            chrome.storage.sync.set({
-                'sites': site_list
-            }, function() {
-                sendResponse({
-                    action_taken: "added"
-                });
+            chrome.storage.sync.set({'sites': site_list}, function() {
+                sendResponse({action_taken: "added"});
+            })
+            return true;
+        });
+        return true;
+    }
+	if (request.remove_site) {
+		console.log("h")
+        chrome.storage.sync.get('sites', function(result) {
+			if (chrome.runtime.lastError){
+				sendResponse({action_taken: "error"});
+				return false
+			}
+            var site_list = result.sites
+			console.log(request.remove_site)
+			site_list.splice(request.remove_site, 1);
+            chrome.storage.sync.set({'sites': site_list}, function() {
+                sendResponse({action_taken: "removed"});
             })
             return true;
         });
@@ -45,57 +61,16 @@ chrome.runtime.onInstalled.addListener(function(details) {
     default_site.push('https://stackoverflow.com')
     default_site.push('https://wired.com')
     default_site.push('https://github.com')
-    console.log(default_site)
-    chrome.storage.sync.set({
-        'sites': default_site
-    }, function() {
-        chrome.storage.sync.set({
-            'num_of_sites': 4
-        }, function() {
-            store_url()
-        })
-    })
+    chrome.storage.sync.set({'sites': default_site}, get_site_url)
 });
-// get the total number of "safe" sites that user has chosen
-// google homepage is the default site
-function get_num_of_sites(callback) {
-    chrome.storage.sync.get('num_of_sites', function(result) {
-        if (!num) {
-            chrome.storage.sync.set({
-                'num_of_sites': 1
-            })
-            chrome.storage.sync.set({
-                'site_1': "https://www.google.ca"
-            })
-            num = 1
-        }
-        if (callback) {
-            callback(result.num_of_sites)
-        }
-    })
-};
 
 // given the total number of site, get the url for one site randomly
 // google homepage is default
 function get_site_url() {
     chrome.storage.sync.get('sites', function(result) {
         var site_list = result.sites
-        console.log(1, site_list)
         var num_of_sites = site_list.length
         var site_number = Math.floor(Math.random() * num_of_sites);
-        chrome.storage.sync.set({
-            'site_url': site_list[site_number]
-        });
+        chrome.storage.sync.set({'site_url': site_list[site_number]});
     });
 };
-
-function store_url() {
-    get_site_url()
-}
-
-function get_sites() {
-    chrome.storage.sync.get('sites', function(result) {
-        var site_list = result.sites
-
-    });
-}
