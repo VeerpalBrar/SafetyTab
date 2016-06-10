6af09f40deb3e2051a87892d4677de2699e67c94
Veerpal Brar <VeerpalBrar@users.noreply.github.com>
2016-06-10 11:24:57 -0400

    Update option.js

--- option.js
+++ option.js
@@ -1,10 +1,13 @@
+
+//add a site to the list of safety sites
 function add_site() {
-    console.log("start")
     var site_url = $('#site_name').val();
     //site_url = format_url(site_url)
-    console.log(site_url)
+	document.getElementById('site_name').value = "";
+	document.getElementById('error').innerHTML = "";
+	document.getElementById('js_error').innerHTML = ""
     if (!check_url_valid(site_url)) {
-        alert("Sorry, that is not a valid website!")
+        console.log("Sorry, that is not a valid website!")
         return
     }
     chrome.runtime.sendMessage({
@@ -14,16 +17,26 @@ function add_site() {
             remove_children('site_list')
             get_urls(add_button_listeners);
         }
+		if (response.action_taken == "error") {
+            document.getElementById('js_error').innerHTML = "Sorry, that site could not be added."
+        }
     });
-    document.getElementById('site_name').value = "";
 }
 
-// not complete. placeholder. 
+// very basic URL validator. Only makes sure that url starts with "https://www." or "http://www."
 function check_url_valid(url) {
-    if (url.charAt(0) == "h") return true
-    return false
+    if (url.substring(0,8) != "https://" && url.substring(0,7)!="http://" ){
+		document.getElementById('error').innerHTML = "Error: Please begin your url with 'https://' or 'http://'.";
+		return false;
+	} 
+	if (url.substring(8,12)!="www." && url.substring(7, 11)!="www."){
+		document.getElementById('error').innerHTML = "Error: Please include 'www.' in your url.";
+		return false;
+	}
+    return true
 }
 
+//gets the list of saved urls and displays them to the user
 function get_urls(callback) {
     chrome.storage.sync.get('sites', function(items) {
         var listOfSites = items.sites
@@ -40,6 +53,7 @@ function get_urls(callback) {
     })
 }
 
+//remove every node from the list of sites
 function remove_children(element_id) {
     var myNode = document.getElementById(element_id);
     while (myNode.firstChild) {
@@ -47,20 +61,25 @@ function remove_children(element_id) {
     }
 }
 
+//remove a site from the list of safety sites
 function remove_site() {
+	document.getElementById('error').innerHTML = "";
+	document.getElementById('js_error').innerHTML = ""
     key = this.id
-    chrome.storage.sync.get('sites', function(items) {
-        var listOfSites = items.sites;
-        listOfSites.splice(key, 1);
-        chrome.storage.sync.set({
-            'sites': listOfSites
-        }, function() {
-            remove_children('site_list');
+    chrome.runtime.sendMessage({
+        remove_site: key
+    }, function(response) {
+        if (response.action_taken == "removed") {
+            remove_children('site_list')
             get_urls(add_button_listeners);
-        });
+        }
+		if (response.action_taken == "error") {
+            document.getElementById('js_error').innerHTML = "Sorry, that site could not be removed."
+        }
     });
 };
 
+// add a click listener for all 'delete' site buttons. 
 function add_button_listeners() {
     var del_buttons = document.querySelectorAll('.delete');
     for (i = 0; i < del_buttons.length; i++) {
